f128d0a2a255b163dda787463e91f897e47b5e78
Aniket Raj <araj52444@gmail.com>
2026-08-13 12:34:03 +0530

    Add initial implementation of hello.py and variable.py with basic examples

---
+++ hello.py
@@ -0,0 +1,19 @@
+# this is a comment
+print("hello world")
+
+"""
+this is a comment
+written in
+multiple lines
+"""
+
+# print(print.__doc__)
+
+def add(a,b):
+    """
+    add two numbers vdsjhmfhopds
+    """
+    print(a+b)
+
+print(add.__doc__)
+print(help(add))
---
+++ variable.py
@@ -0,0 +1,58 @@
+name = "Aniket" #string
+age = 20 #interger
+f = 1.1 #float
+c = 4 + 5j #complex number
+
+# dynamically typed language
+# a = "A"
+# a = 1
+# print(a)
+
+"""
+1. string - str
+2. integer - int
+3. decimal numbers - float
+4. complex number - complex (REAL + IMAGINARY)
+5. Boolean - bool
+6. None - None
+"""
+
+
+#for String
+#double quote
+city = "noida"
+#single quote
+country = 'india'
+#triple (single/double)quote
+college = """GLA University"""
+college = '''GLA University'''
+
+#Numbers
+#int
+year = 2026
+#float
+pi = 3.14
+#complex
+comp = 4 + 5j
+
+#Boolean
+success = True
+failed = False
+
+# print(type(comp))
+# print(type(success))
+
+# print(isinstance(failed, bool))
+
+"""
+str()
+int()
+float()
+complex()
+bool()
+"""
+
+
+str_year = str(year)
+print(complex(year))
+print(bool(456))
