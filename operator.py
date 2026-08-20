9a12ba2d2a5e7b8d1af359d8420f53b0fc20d305
Aniket Raj <araj52444@gmail.com>
2026-08-20 16:07:38 +0530

    Add operator.py with examples of arithmetic, assignment, comparison, and logical operators

---
+++ operator.py
@@ -0,0 +1,72 @@
+#opeartion
+#operand
+#operator
+
+#3 + 12
+
+"""
+Operators Types
+1. Arithmetic Operators
+2. Assignment Operators
+3. Comparison Operators
+4. Logical Operators
+"""
+
+#Arithmetic Operators(+, -, *, /, %, //, **)
+n1 = 2
+n2 = 4
+add = n1 + n2
+sub = n1 - n2
+mul = n1 * n2
+div = n1 / n2
+remainder = n1 % n2
+floor_div = n1 // n2
+power = n1 ** n2
+
+# print("Reaminder",remainder)
+# print("Floor Division",floor_div)
+# print("Power",power)
+
+# Assignment Operators ( = )
+name = "Aniket"
+n = 2
+n = n + 1
+n += 1
+# print(n)
+
+# Comparison Operators ( == , != , > , < , >= , <= )
+n1 = 2
+n2 = 4
+# print(n1 == n2)
+# print(n1 != n2)
+# print(n1<n2)
+# print(n1>n2)
+# print(n1<=n2)
+# print(n1>=n2)
+
+# Logical operators (not, and, or)
+n1 = 2
+n2 = 4
+
+s1 = True
+s2 = False
+
+# print("S1",not s1)
+# print("S2",not s2)
+# print(True and False and True)
+# print("Aniket" and "" and 0)
+
+# print(1>2 or 0)
+
+#priority (not -> and -> or)
+# print(not False and "Hii" or True)
+# True and "hii" or True
+# "hii" or true
+
+print(not True and "Hii" or True)
+
+# (False and "Hii") or True
+# False or True
+# True
+
+print(not True and "Hii" or None or "Hello" )
