36dfdf45767ad29b29affb5a5f85d66785d47aab
Aniket Raj <araj52444@gmail.com>
2026-08-27 10:57:52 +0530

    Refactor lists.py to enhance list operations examples and add sorting functionality

--- lists.py
+++ lists.py
@@ -27,11 +27,74 @@ del items[1]
 n = [1,2,3]
 
 n.insert(2, "Aniket")
-print(n)
+# print(n)
 #1,2,"Aniket",3
 n.pop(-2)
 # print(n)
 
 d = [1,2,3,2,4,2]
 d.remove(2)
-print(d)
+# print(d)
+
+# CREATE
+# append(value) -> add value at the end
+# insert(index, value) -> add value at the given index
+
+# DELETE
+# pop(index = -1) -> remove value at the given index
+# remove(value) -> remove first matched value
+# del items[index] -> keyword
+
+first_index = 0 or -len(items)
+last_index = -1 or len(items) - 1
+
+
+it = [1,2,3,4,3,5,6,3,2]
+# print(it.count(4)) #count(value) -> return count of given value
+
+#index(value, start = 0, stop = len(list))
+# print(it.index(2,-2))
+# print(it.index(3,0,6))
+
+
+l = [1,2,3]
+
+# reverse() -> reverse the list
+# l.reverse()
+# print(l)
+
+# l.sort()
+# print(l)
+
+# x = [64,45,52,5,8,99,2]
+# x.sort(reverse=True)
+# print(x)
+
+# y = ["t","D","a","A","T"]
+# y.sort(reverse=True)
+# print(y)
+
+names = ["Shivang","Aniket", "Pulkit","Aakash", "Mayank", "Riddhi"]
+# names.sort(key=len, reverse=True)
+# print(names)
+
+# z = [-562, -203, 197, 436, -170, -64]
+
+# z.sort(key=abs)
+# print(z)
+
+# abs(-10) #10
+
+b = [3,2,4,1,6,5]
+
+def aniket(abs):
+    # print(abs % 2 ==0 and abs + 1)
+    if(abs % 2 == 0):
+        return abs + 1
+    else:
+        return abs
+
+new_list = sorted(b, key=aniket)
+print("old list", b)
+print("normal",sorted(b))
+print("New List", new_list)
