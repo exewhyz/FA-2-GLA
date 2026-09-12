fbe48a4acf1286707a708aa2d97aac7984058176
Aniket Raj <araj52444@gmail.com>
2026-09-12 15:07:50 +0530

    Add loops.py with examples of for and while loops

---
+++ loops.py
@@ -0,0 +1,71 @@
+# Loop Works on List,Tuple,String
+
+# Type of loops
+# 1. for loop
+# 2. while loop
+
+text = "Python Programming"
+# print(text.upper())
+# EVEN INDEX chars (P,t,o," ",r,g,a,m,n)
+
+# for value in text:
+#     print(value)
+
+
+# nums= [1,2,3,4,5]
+# for num in nums:
+#     print(num)
+    
+    
+
+"""
+SYNTAX
+
+for value_name in str/list/tuple_name:
+    #logic
+"""
+
+# n = [54, 6, 9, 3, 2]
+
+# for value in n:
+#     if value % 2 == 0:
+#         print(value,"is Even")
+#     else:
+#         print(value, "is odd")
+
+
+# len()
+# type()
+# print()
+# input()
+# sum()
+# max()
+# min()
+# abs()
+# int()
+# float()
+# complex()
+# str()
+# sorted()
+
+
+# range(start,stop,step) -> int
+
+# print(range(6))
+# print(list(range(6)))
+# print(list(range(2,6)))
+# print(type(range(6)))
+
+# for i in range(11):
+#     print(i)
+    
+# for value in range(1,101):
+#     value % 2 == 0 and print(value)
+
+# for value in range(2,101,2):
+#     print(value)
+    
+for ch in "hello":
+    print(ch)
+    
+# write a program to check if student got less than 50 marks in any of the subjects.
